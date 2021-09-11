1fc6bb6aba068cf5afee3484c05437db642f196b
ldiebold <luke@ldiebold.com>
2021-09-11 13:17:56 +1000

    all headers

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -107,9 +107,7 @@ return [
              *
              * To store all headers, set this value to `*`.
              */
-            'store_headers' => [
-
-            ],
+            'store_headers' => '*',
 
             /*
              * The class name of the job that will process the webhook request.
