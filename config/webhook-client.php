c63448de44ce362408b559f3317c14e4cb5163dc
ldiebold <luke@ldiebold.com>
2021-09-11 12:53:49 +1000

    WIP

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -25,7 +25,7 @@ return [
              *
              * It should implement \Spatie\WebhookClient\SignatureValidator\SignatureValidator
              */
-            'signature_validator' => \App\Http\Webhooks\DropboxWebhookSignatureValidator::class,
+            'signature_validator' => \App\Http\Webhooks\DropboxAuthWebhookSignatureValidator::class,
 
             /*
              * This class determines if the webhook call should be stored and processed.
